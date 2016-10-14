6a1a2459eafcfbddd4ed8f4c789c2c12a561833c
Alessandro Giannini <alessandro@develer.com>
2016-10-14 16:35:42 +0200

    Force section headers background color

--- RNTableView/RNTableView.m
+++ RNTableView/RNTableView.m
@@ -17,6 +17,12 @@
 #import "RNTableHeaderView.h"
 #import "RNReactModuleCell.h"
 
+#define UIColorFromRGB(rgbValue) \
+[UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+                green:((float)((rgbValue & 0x00FF00) >>  8))/255.0 \
+                 blue:((float)((rgbValue & 0x0000FF) >>  0))/255.0 \
+                alpha:1.0]
+
 @interface RNTableView()<UITableViewDataSource, UITableViewDelegate> {
     id<RNTableViewDatasource> datasource;
 }
@@ -287,6 +293,8 @@ RCT_NOT_IMPLEMENTED(-initWithCoder:(NSCoder *)aDecoder)
     if (self.headerFont){
         header.textLabel.font = self.headerFont;
     }
+
+    header.backgroundView.backgroundColor = UIColorFromRGB(0xFFE0D0);
 }
 
 -(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
